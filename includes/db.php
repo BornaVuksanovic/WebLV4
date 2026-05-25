881d8aa1b59e53c70843362b44b6ccf6a4f1b145
BornaVuksanovic <boco.vuksanovic2@gmail.com>
2026-05-25 10:47:34 +0200

    modify db.php

--- includes/db.php
+++ includes/db.php
@@ -1,15 +1,14 @@
 <?php
-$host = "localhost";
-$username = "root";
-$password = "";
-$database = "videoteka_baza"; 
 
-// Kreiranje konekcije (bez onih cite oznaka!)
-$con = mysqli_connect($host, $username, $password, $database);
+$host = getenv('MYSQLHOST') ?: 'localhost';
+$user = getenv('MYSQLUSER') ?: 'root';
+$pass = getenv('MYSQLPASSWORD') ?: '';
+$baza = getenv('MYSQLDATABASE') ?: 'videoteka_baza'; 
+$port = getenv('MYSQLPORT') ?: 3306;
 
-// Provjera konekcije
-if (mysqli_connect_errno()) {
-    echo "Greška pri spajanju na MySQL: " . mysqli_connect_error();
-    die(); 
+$con = mysqli_connect($host, $user, $pass, $baza, $port);
+
+if (!$con) {
+    die("Greška pri spajanju na bazu: " . mysqli_connect_error());
 }
 ?>
